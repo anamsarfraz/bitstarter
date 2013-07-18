3c83cf63739ed282a608340310f353f2cc191fce
anamsarfraz <anamsarfraz@gmail.com>
2013-07-18 16:21:28 +0000

    Updating grader and checks

--- grader.js
+++ grader.js
@@ -7,6 +7,7 @@ basic DOM parsing.
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require ('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -19,23 +20,33 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtmlFile = function(htmlfile, is_url) {
+    if (is_url) {
+        return cheerio.load(htmlfile);
+    } else {
+        return cheerio.load(fs.readFileSync(htmlfile));
+    }
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(htmlfile, checksfile, is_url) {
+    $ = cheerioHtmlFile(htmlfile, is_url);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for (var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
-    return out;
+    console.log(JSON.stringify(out, null, 4));
+};
+
+var checkUrlData = function(url, checksfile) {
+    rest.get(url).on('complete', function(data) {
+        checkHtmlFile(data, checksfile, true);
+    });
 };
 
 var clone = function(fn) {
@@ -48,10 +59,14 @@ if (require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url to download index from')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (program.url) {
+        checkUrlData(program.url, program.checks);
+    } else {
+        checkHtmlFile(program.file, program.checks, false);
+    } 
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }               
